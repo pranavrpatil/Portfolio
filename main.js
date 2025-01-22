3f4092425df24d3203c5b2fd9143d65a23e20ae7
pranavrpatil <patilpranav22221081@gcoeara.ac.in>
2025-01-22 07:47:11 +0530

    Add more functionalities to menu button

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.getElementById('menu-icon');
     const navList = document.querySelector('.navList');
     const navLinks = document.querySelectorAll('.navList a');
-
-    menuIcon.addEventListener('click', function () {
+    const body = document.querySelector('body');
+    
+    menuIcon.addEventListener('click', function (event) {
+        // event.stopPropagation();
         if (navList.style.display === 'block') {
             navList.style.display = 'none';
         } else {
@@ -16,6 +18,12 @@ document.addEventListener('DOMContentLoaded', function () {
         link.addEventListener('click', function () {
             navList.style.display = 'none';
         });
+    });
+
+    body.addEventListener('click', function (event) {
+        if (navList.style.display === 'block' && !navList.contains(event.target) && event.target !== menuIcon) {
+            navList.style.display = 'none';
+        }
     });
 
 });
